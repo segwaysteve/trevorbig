a33e7fe2ce825c139500907d96789e49dbcb7ab8
guoste20 <stevewangguo@gmail.com>
2020-02-10 08:48:27 -0800

    first

--- app/src/main/java/com/example/trevorbig/AddFit.java
+++ app/src/main/java/com/example/trevorbig/AddFit.java
@@ -2,13 +2,53 @@ package com.example.trevorbig;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddFit extends AppCompatActivity {
+    AutoCompleteTextView Workout;
+    EditText Date;
+    EditText Time;
+    EditText Notes;
+    Button submit;
+    Integer DefaultColor;
+    Button cancel;
+    Button OpenColorPicker;
+    ArrayList emotionArray;
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_fit);
+
+        EditText autoDate = (EditText)findViewById(R.id.Date);
+        EditText autoTime = (EditText)findViewById(R.id.Time);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a", Locale.getDefault());
+        String date = dateFormat.format(Calendar.getInstance().getTime());
+        String time = timeFormat.format(Calendar.getInstance().getTime());
+
+        autoDate.setText(date);
+        autoTime.setText(time);
+
+        cancel = (Button) findViewById(R.id.CancelButton);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddFit.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/trevorbig/DBHelper.java
@@ -0,0 +1,165 @@
+package com.example.trevorbig;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "swag.db";
+    public static final String TABLE_NAME = "GETBIG";
+    public static final String col_1 = "ID";
+    public static final String col_2 = "workout";
+    public static final String col_3 = "color";
+    public static final String col_4 = "date";
+    public static final String col_5 = "time";
+    public static final String col_6 = "notes";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + "workout TEXT,"
+                + "color TEXT," + "date TEXT," + "time TEXT," + "notes TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+
+    public boolean addWorkout(String workout, Integer color, String date, String time, String notes) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_2, workout);
+        contentValues.put(col_3, color);
+        contentValues.put(col_4, date);
+        contentValues.put(col_5, time);
+        contentValues.put(col_6, notes);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+
+    public void updateWorkout (Integer id, String workout, Integer color, String date, String time, String notes) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_2, workout);
+        contentValues.put(col_3, color);
+        contentValues.put(col_4, date);
+        contentValues.put(col_5, time);
+        contentValues.put(col_6, notes);
+        db.update("Workout", contentValues, col_1 + " = " + id, null);
+    }
+
+    public void deleteWorkout(Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NAME +
+                " WHERE " + col_1 + " = " + id);
+        db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE " + id + " = " + id);
+    }
+
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME +
+                " where " + col_1 + " = " + id + " ", null);
+        return res;
+    }
+
+    public String getWorkout(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME +
+                " where " + col_1 + " = " + id + " ", null);
+        String workout= "nothing";
+        if (res.moveToFirst()) {
+            workout = res.getString(res.getColumnIndex(col_2));
+        }
+        return workout;
+    }
+
+    public Integer getColor(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME +
+                " where " + col_1 + " = " + id + " ", null);
+        int color = 0;
+        if (res.moveToFirst()) {
+            color = res.getInt(res.getColumnIndex(col_3));
+        }
+        return color;
+    }
+
+    public String getModeEmotion() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.query(TABLE_NAME, new String[] {col_1, col_2, col_3, col_4, col_5, col_6}, null,
+                null, col_2, "COUNT (*) = ( SELECT MAX(Cnt) FROM( SELECT COUNT(*) as Cnt FROM " + TABLE_NAME + " GROUP BY " + col_2 + " ) tmp )",
+                null, null);
+        String mode = null;
+        if (res.moveToFirst()) {
+            mode = res.getString(res.getColumnIndex(col_2));
+        }
+        return mode;
+    }
+
+    public String getModeReason() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.query(TABLE_NAME, new String[] {col_1, col_2, col_3, col_4, col_5, col_6}, null,
+                null, col_6, "COUNT (*) = ( SELECT MAX(Cnt) FROM( SELECT COUNT(*) as Cnt FROM " + TABLE_NAME + " GROUP BY " + col_6 + " ) tmp )",
+                null, null);
+        String mode = null;
+        if (res.moveToFirst()) {
+            mode = res.getString(res.getColumnIndex(col_6));
+        }
+        return mode;
+    }
+
+    public ArrayList<String> getAllDistinctEmotions() {
+        ArrayList<String> array_list = new ArrayList<String>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor res = db.query(true, TABLE_NAME, new String[] {col_1, col_2, col_3, col_4, col_5, col_6},
+                null, null, col_2, null, null, null);
+        res.moveToFirst();
+
+        while (res.isAfterLast() == false) {
+            array_list.add(res.getString(res.getColumnIndex(col_2)));
+            res.moveToNext();
+        }
+
+        return array_list;
+    }
+
+    public ArrayList<String> getDayEmotions(String date) {
+        ArrayList<String> array_list = new ArrayList<String>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor res = db.rawQuery("select * from Emotion", null);
+        res.moveToFirst();
+
+        while (res.isAfterLast() == false) {
+            if (res.getString(res.getColumnIndex(col_4)).equals(date)) {
+                array_list.add(res.getString(res.getColumnIndex(col_2)));
+            }
+            res.moveToNext();
+        }
+
+        return array_list;
+    }
+
+}
+
+
